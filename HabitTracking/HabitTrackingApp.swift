eaf09961741adc00e8d946a72969135fd246061f
Leah Somerville <leah.somerville4@gmail.com>
2024-03-21 09:52:10 -0500

    Initial Commit

---
+++ HabitTracking/HabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackingApp.swift
+//  HabitTracking
+//
+//  Created by Leah Somerville on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
